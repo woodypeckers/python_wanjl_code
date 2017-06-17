57adf1e1ba57026872d20e3feb42c5651fc623dc
18576672691 <987184308@qq.com>
2017-06-17 16:08:45 +0800

    second commit

---
+++ ex01_for.py
@@ -0,0 +1,13 @@
+#encoding:utf-8
+from libs.sayHello  import sayHello
+fruits=["banana","apple","pear"]
+
+
+for fruit in fruits:
+    print "水果：%s" %fruit
+    
+    
+for letter in "python":
+    print "字母是：%s" %letter
+    
+    sayHello()
